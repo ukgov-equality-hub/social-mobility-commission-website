0f5db3b7fc747e1c2573fcc169779f3ce72f3efe
James Griffiths <james@jwgsoftware.com>
2023-12-12 14:17:15 +0000

    EHD-833: Setup SMC website hosting: Terraform: Redirect from www domain to root domain

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -16,3 +16,17 @@ resource "aws_route53_record" "dns_alias_record__main_website" {
     zone_id = aws_cloudfront_distribution.distribution__main_website.hosted_zone_id
   }
 }
+
+resource "aws_route53_record" "dns_alias_record__www_domain_redirect" {
+  count = var.create_redirect_from_www_domain ? 1 : 0  // Only create this DNS record if "var.create_redirect_from_www_domain" is true
+
+  zone_id = data.aws_route53_zone.route_53_zone_for_our_domain.zone_id
+  name    = "${var.dns_record_www_domain_including_dot}${data.aws_route53_zone.route_53_zone_for_our_domain.name}"
+  type    = "A"
+
+  alias {
+    evaluate_target_health = false
+    name = aws_cloudfront_distribution.distribution__www_domain_redirect[0].domain_name
+    zone_id = aws_cloudfront_distribution.distribution__www_domain_redirect[0].hosted_zone_id
+  }
+}
